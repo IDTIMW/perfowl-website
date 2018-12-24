2839550db7b91afce9a61bdea216809d3203b3aa
w <tim.xing.wu@gmail.com>
2018-12-24 16:18:51 +0800

    默认配置 + material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.ts
@@ -0,0 +1,15 @@
+import { MatButtonModule, MatToolbarModule } from '@angular/material';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    imports: [
+        MatButtonModule,
+        MatToolbarModule,
+    ],
+    exports: [
+        MatButtonModule,
+        MatToolbarModule
+    ]
+})
+
+export class MaterialModule { }
