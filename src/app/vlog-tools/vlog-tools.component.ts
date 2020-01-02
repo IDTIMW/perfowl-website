5c0279bebd94236e5eee5b282bdbed46555c1cdc
TIMW <tim.xing.wu@gmail.com>
2020-01-02 22:04:20 +0800

    =fixed router error in vlog-tools.ts file

--- src/app/vlog-tools/vlog-tools.component.ts
+++ src/app/vlog-tools/vlog-tools.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SiteService } from 'src/app/site.service';
 import { SeoService } from 'src/app/seo.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-vlog-tools',
@@ -21,6 +22,7 @@ export class VlogToolsComponent implements OnInit {
 
 
   constructor(
+    private router: Router,
     private _Sites: SiteService,
     private seoService: SeoService,
   ) { }
